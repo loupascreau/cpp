7fa27a8dd604ba356a27ecd27ddb2f83c2c0f6d6
lou pascreau <loupascreau@gmail.com>
2021-12-23 14:16:20 +0100

    cpp07 ex02 finished

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: lpascrea <lpascrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 11:14:54 by lpascrea          #+#    #+#             */
-/*   Updated: 2021/12/23 11:45:55 by lpascrea         ###   ########.fr       */
+/*   Updated: 2021/12/23 14:10:54 by lpascrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,80 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include <stdexcept>
+# define MAX_VAL 750
 
 template <typename T>
 class Array
 {
 	public :
 	
-	Array<T>(void) : _array(new T[0]) {};
-	Array<T>(unsigned int n) : _array(new T[n]) {};
-//	Array<T>(Array<T> const & obj) {};
-	~Array<T>(void) {};
+	Array<T>(void) : _size(0), _array(NULL)
+	{
+		std::cout << "Default Constructor called" << std::endl;
+	}
+	
+	Array<T>(unsigned int n) : _size(n), _array(new T[n])
+	{
+		std::cout << "Array Constructor called, size = " << this->_size << std::endl;
+	}
+
+	Array<T>(Array<T> const & obj)
+	{
+		int		i;
+
+		i = 0;
+		std::cout << "Copy Constructor called, size = " << obj.getSize() << std::endl;
+		this->_size = obj.getSize();
+		this->_array = new T[this->_size];
+		while (i < this->_size)
+		{
+			this->_array[i] = obj._array[i];
+			i++;
+		}
+	}
+	
+	Array<T> &	operator=(Array<T> const & obj)
+	{
+		int		i;
+
+		i = 0;
+		std::cout << "Assignement operator called" << std::endl;
+		if (this == &obj)
+			return *this;
+		if (this->_size > 0)
+			delete [] this->_array;
+		this->_size = obj.getSize();
+		this->_array = new T[this->_size];
+		while (i < this->_size)
+		{
+			this->_array[i] = obj._array[i];
+			i++;
+		}
+		return *this;
+	}
+
+	T &	operator[](int i)
+	{
+		if (i < 0 || i >= this->_size)
+			throw std::overflow_error("Error : Trying to access an invalid index");
+		return this->_array[i];
+	}
+	
+	~Array<T>(void)
+	{
+		std::cout << "Destructor called" << std::endl;
+		if (this->_size > 0)
+			delete [] this->_array;
+		
+	}
+
+	T	*getArray(void) const { return this->_array; };
+	int	getSize(void) const { return this->_size; };
 
 	private :
 
+	int	_size;
 	T	*_array;
 };
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lpascrea <lpascrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 11:50:12 by lpascrea          #+#    #+#             */
-/*   Updated: 2021/12/23 11:44:01 by lpascrea         ###   ########.fr       */
+/*   Updated: 2021/12/23 14:13:35 by lpascrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,70 @@
 
 int		main(void)
 {
-	Array<char>	char_array(2);
-	Array<int>	int_array(4);
-	
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+
+	/**********Filling data**********/
+	std::cout << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+	std::cout << "Filling up values" << std::endl;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+
+	/*******Values Verification*******/
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+	std::cout << "It's ok values are equals" << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+
+    /*************Scope***************/
+    {
+		std::cout << std::endl;
+		std::cout << "----------Inside a scope---------" << std::endl;
+        Array<int> tmp = numbers;
+		Array<int> test(tmp);
+    }
+	std::cout << "---------------------------------" << std::endl;
+	std::cout << std::endl;
+
+	/***********exceptions**********/
+	std::cout << "------------Exceptions-----------" << std::endl;
+	try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+	try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+	std::cout << "---------------------------------" << std::endl;
+	std::cout << std::endl;
+
+	/******************************/
+	for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;
+
 	return 0;
 }
